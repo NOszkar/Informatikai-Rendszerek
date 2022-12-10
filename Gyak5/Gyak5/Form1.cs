cb9ec22306a0c8536af6586bb69f4554efd8d3e5
NOszkar <oszkar.nagy@stud.uni-corvinus.hu>
2022-12-10 14:26:29 +0100

    Chart added

--- Gyak5/Gyak5/Form1.cs
+++ Gyak5/Gyak5/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace Gyak5
@@ -23,6 +24,7 @@ namespace Gyak5
             string result = RequestData();
             ProcessXML(result);
             dataGridView1.DataSource = Rates;
+            showDiagram(result);
         }
 
         public string RequestData()
@@ -71,6 +73,25 @@ namespace Gyak5
                     rate.Value = value / unit;
             }
 
+        }
+
+        public void showDiagram(string result)
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
